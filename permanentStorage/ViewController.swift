f0e06563401d2ae365a7749992013d54cf0370bb
Andrew Morrison <morro141@gmail.com>
2016-02-28 20:39:20 -0400

    Simple permanent storage, final commit

--- permanentStorage/ViewController.swift
+++ permanentStorage/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //NSUserDefaults.standardUserDefaults().setObject("Andrew", forKey: "name")
+        
+        var name = NSUserDefaults.standardUserDefaults().objectForKey("name")! as! String //needs to be explicitly unwrapped
+        
+        print(name)
+        
+        var arr = [1,2,3]
+        
+        //NSUserDefaults.standardUserDefaults().setObject(arr, forKey: "array")
+        var recalledArray = NSUserDefaults.standardUserDefaults().objectForKey("array")! as! NSArray
+        
+        print(recalledArray)
+        
     }
 
     override func didReceiveMemoryWarning() {
